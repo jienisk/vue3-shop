57dd7adda4ba4d0db09aaf1359e02ee8921e349c
jienisk <jienisk@outlook.com>
2025-03-16 00:46:39 +0800

    henduo

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +26,10 @@ const router = createRouter({
           path: 'category/sub/:id',
           component: SubCategory,
         },
+        {
+          path: 'detail/:id',
+          component: Detail
+        }
       ]
     },
     {
@@ -31,6 +37,13 @@ const router = createRouter({
       component: Login,
     }
   ],
+
+   scrollBehavior(){
+    return {
+      top:0
+    }
+   }
+
 })
 
 export default router
--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -35,6 +35,16 @@ const tabChange = ()=>{
     
 }
 
+const disabled = ref(false)
+const load = async () => {
+    reqData.value.page++
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value = [...goodList.value,...res.result.items]
+    if(res.result.items.length === 0){
+        disabled.value = true
+    }
+}
+
 </script>
 
 <template>
@@ -54,7 +64,7 @@ const tabChange = ()=>{
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                  <GoodsItem v-for="item in goodList" :key="item.id" :goods="item"></GoodsItem>
             </div>
