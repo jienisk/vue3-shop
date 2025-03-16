13e6c3911bc035d64ed704a2eeb57c220867bd19
jienisk <jienisk@outlook.com>
2025-03-17 00:46:15 +0800

    放大镜

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 
 
 // 图片列表
@@ -17,6 +18,41 @@ const enterhandler = (i) => {
     activeIndex.value = i
 }
 
+// 获取鼠标相对位置
+const target = ref(null)
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+//控制滑块跟随鼠标移动
+const left = ref(0)
+const top = ref(0)
+const positionX = ref(0)
+const positionY = ref(0)
+watch([elementX, elementY,isOutside],() => {
+  if(isOutside.value) return
+   if(elementX.value && elementY.value < 300){
+     left.value = elementX.value - 100
+   }
+   if(elementY.value > 100 && elementY.value < 300){
+    top.value = elementY.value - 100
+   }
+
+   if(elementX.value > 300){
+     left.value = 300 - 100
+   }
+   if(elementX.value < 100){
+     left.value = 100 - 100
+   }
+   if(elementY.value > 300){
+     top.value = 300 - 100
+   }
+   if(elementY.value < 100){
+     top.value = 100 - 100
+   }
+
+   positionX.value = -left.value * 2
+   positionY.value = -top.value * 2
+
+})
 
 </script>
 
@@ -28,7 +64,7 @@ const enterhandler = (i) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -40,10 +76,10 @@ const enterhandler = (i) => {
     <div class="large" :style="[
       {
         backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
