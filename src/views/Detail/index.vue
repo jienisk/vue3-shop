354fe889811c3623ee13f93f3e780a6c53b872bc
jienisk <jienisk@outlook.com>
2025-03-16 17:40:29 +0800

    asda

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import DetailHot from './components/DetailHot.vue'
 import { getDetail } from '@/apis/detail'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
@@ -116,7 +117,10 @@ onMounted(()=>getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <!-- 24小时 -->
+              <DetailHot :hot-type="1"/>
+              <!-- 周 -->
+              <DetailHot :hot-type="2"/>
             </div>
           </div>
         </div>
