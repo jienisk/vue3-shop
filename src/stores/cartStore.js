06a0c8f90ce59aafe4cd6c2d434759c214560577
jienisk <jienisk@outlook.com>
2025-03-18 23:38:02 +0800

    gouwuche

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 
 export const useCartStore = defineStore('cart',() =>{
@@ -14,9 +14,27 @@ export const useCartStore = defineStore('cart',() =>{
       cartList.value.push(goods)
     }
     }
+
+    //删除购物车
+    const delCart = (skuId) => {
+        // 思路：
+      // 1. 找到要删除项的下标值 - splice
+      const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+      cartList.value.splice(idx, 1)
+    }
+
+    const allCount = computed(() => 
+        cartList.value.reduce((pre, cur) => pre + cur.count, 0)
+  )
+    const allPrice = computed(() => 
+        cartList.value.reduce((pre, cur) => pre + cur.count * cur.price, 0)
+    )
     return {
         cartList,
-        addCart
+        addCart,
+        delCart,
+        allCount,
+        allPrice
     }
 },{
     persist: true,
