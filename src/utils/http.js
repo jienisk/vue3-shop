d8ba63cc6f98ca16559b5e4a9e50520082dbcc0e
jienisk <jienisk@outlook.com>
2025-03-18 18:33:07 +0800

    login end

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
     baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -9,16 +11,26 @@ const httpInstance = axios.create({
 
 // 请求拦截器
 httpInstance.interceptors.request.use(config => {
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
     // 在发送请求之前做一些处理
     return config
 }, e => Promise.reject(e))
 
 // 响应拦截器   
 httpInstance.interceptors.response.use(res => res.data, e => {
+    const userStore = useUserStore()
     ElMessage({
         type: 'warining',
         message: e.response.data.message
     })
+    if(e.response.status === 401){
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
