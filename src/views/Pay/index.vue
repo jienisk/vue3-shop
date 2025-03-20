5981512c148746d314c0e4554b93cfe3f5b6b500
jienisk <jienisk@outlook.com>
2025-03-20 22:35:52 +0800

    pay

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,5 +1,26 @@
 <script setup>
-const payInfo = {}
+import { getOrderAPI } from '@/apis/pay'
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { useCountDown } from '@/composables/useCountDown';
+
+const { formaTime, start } = useCountDown()
+const route = useRoute();
+const payInfo = ref({})
+const getPayInfo =async () => {
+    const res = await getOrderAPI(route.query.id)
+    payInfo.value = res.result
+    start(res.result.countdown)
+}
+
+onMounted(() => getPayInfo())
+
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
+
 </script>
 
 
@@ -11,7 +32,7 @@ const payInfo = {}
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>支付还剩 <span>{{ formaTime }}</span>, 超时后将取消订单</p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
