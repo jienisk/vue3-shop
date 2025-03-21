9898d653674d54bc53d8abbf789f3ca493a2cccc
jienisk <jienisk@outlook.com>
2025-03-22 00:39:18 +0800

    jiba

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -16,11 +16,5 @@ import HomePanel from './components/HomePanel.vue'
   <HomeHot />
   <homeProduct />
 
-  <HomePanel title="新鲜好物" sub-title="精选好物 限时抢购">
-    <div>asdsa</div>
-  </HomePanel>
 
-  <HomePanel title="人气推荐" sub-title="热门商品 精选好货">
-    <div>asdasd</div>
-  </HomePanel>
 </template>
